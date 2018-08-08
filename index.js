d995e5be060d59cb785808d20c8442b2e4723ae9
Yuval Saraf <unimonkiez@gmail.com>
2018-08-08 15:09:01 +0300

    WAY less configuration and emitting files with webpack

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-const dts = require('dts-bundle');
-const path = require('path');
-const rimraf = require('rimraf');
+const { generateDtsBundle } = require('dts-bundle-generator');
+const loaderUtils = require('loader-utils');
+
+const runTest = (test, value) => {
+  if (test instanceof RegExp) {
+    return test.test(value);
+  } else if (typeof test === 'function') {
+    return test(value);
+  } else {
+    throw new Error('options.test needs to be regex or function.');
+  }
+}
 
 module.exports = class DtsBundlePlugin {
-	constructor({ libName, typingsDir, outputDir, deleteSource = true }) {
-		this.typingsDir = typingsDir;
-		this.outputDir = outputDir;
-		this.libName = libName;
-		this.deleteSource = deleteSource;
+	constructor({ test = /\.tsx?$/, name = '[name].d.ts' } = {}) {
+		this.test = test;
+		this.name = name;
 	}
 
 	apply(compiler) {
-		console.log(this.libName);
 		compiler.plugin('emit', (compilation, callback) => {
 			compilation.chunks.forEach((chunk) => {
-				chunk.forEachModule((module) => {
-					if (module.resource) {
-						const file = path.parse(module.resource);
-						if (file.ext === '.ts' || file.ext === '.tsx') {
-							dts.bundle({
-								name: this.libName,
-								main: `${this.typingsDir}/${file.name}.d.ts`,
-								out: `${this.outputDir}/${chunk.name}.d.ts`,
-								removeSource: false,
-								outputAsModuleFolder: true
-							});
-						}
+				for (const module of chunk.modulesIterable) {
+					if (module.issuer && module.issuer.depth === 0 && runTest(this.test, module.resource)) {
+						const dts = generateDtsBundle(module.resource);
+						const dtsName = loaderUtils.interpolateName(
+							{
+								resourcePath: `./${chunk.name}.js`,
+							},
+							this.name,
+							{},
+						);
+
+						compilation.assets[dtsName] = {
+							source() {
+								return dts;
+							},
+							size() {
+								return dts.length;
+							}
+						};
 					}
-				});
-			});
-			if (this.deleteSource) {
-				rimraf(this.typingsDir, callback);
-			} else {
-				callback();
-			}
+        		}
+     		});
+      		callback();
 		});
 	}
 };
